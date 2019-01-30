345c3e906b642908feb5d92394d10862bf935f0b
sWIFchbebs <christian@bebst.de>
2019-01-30 15:47:04 +0100

    sWIFchbebs

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -3,7 +3,33 @@ package ueb01;
 class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+		CharStackImpl csLeft = new CharStackImpl();
+		CharStackImpl csRight = new CharStackImpl();
+		char[] cArray = s.replaceAll(" ", "").toLowerCase().toCharArray();
+		boolean r = true, ungleich = false;
 
-		return false;
+		if(cArray.length == 1) return r;
+
+		int anzahlMitte;
+		if(cArray.length % 2 == 0){
+			anzahlMitte = cArray.length / 2;
+		} else {
+			anzahlMitte = (int) cArray.length / 2;
+			ungleich = true;
+		}
+
+		for(int i = 0; i < anzahlMitte; i++) {
+			csLeft.push(cArray[i]);
+		}
+		if(ungleich == true) anzahlMitte++;
+		for(int i = cArray.length - 1; i >= anzahlMitte; i--) {
+			csRight.push(cArray[i]);
+		}
+
+		for(int i = 0; i < csLeft.size(); i++) {
+			if(csLeft.pop() != csRight.pop()) r = false;
+		}
+
+		return r;
 	}
 }
